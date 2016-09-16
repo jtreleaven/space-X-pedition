c0c38e076b69d69051ceb2497faf2387a2b1ebcd
Walker Langley <walkerlangley@gmail.com>
2016-09-16 09:59:00 -0400

    pushing up initial react app

---
+++ reactApp/src/containers/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Welcome to Outer Space</div>
+    )
+  }
+}
+
+export default App;
---
+++ reactApp/webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3333',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    inline: true,
+    port: 3333
+  },
+  module: {
+    loaders:[{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react']
+      },
+      plugins: [
+        'transform-decorators-legacy'
+      ]
+    }]
+  }
+}
